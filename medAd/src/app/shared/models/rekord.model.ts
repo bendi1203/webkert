2d381ad19722ee2e96d21de9a6c035cb2d5d4fcc
boss1203 <h983246@stud.u-szeged.hu>
2021-05-24 11:29:45 +0200

    Rekord megcsinálva

--- medAd/src/app/shared/models/rekord.model.ts
+++ medAd/src/app/shared/models/rekord.model.ts
@@ -1,8 +1,19 @@
 export interface Rekord {
-    id: number;
-    name: string;
-    description?: string;
-    dosage: string;
-    reasonReference: string;
-    device: string;
+    identifier: number; //azonosító
+    status: string; //állapot: folyamatban, várakozik, kész, hibás, ismeretlen
+    statusReason?: string; //állapotindoklás
+    category?: string; //kategória
+    medication: string; //gyógyszer neve
+    subject: string; //embercsoport aki kapja a gyógyszert
+    context?: string; //beteg hányszor járt orvosnál, javult, nem javult..
+    supportInformation?: string; //plusz infók, pl magasság, testsúly stb..
+    effective: string; //dátum, mettől meddig tartott, nem tartott a kezelés
+    perfomer: string; //ki az orvos
+    reasonCode?: string; //indok, hogy miért lett kiírva a gyógyszer
+    reasonReference?: string; //állapot - megfigyelés ami indokolja a gyógyszert
+    request?: string; //hivatalos igénylés
+    device: string; //igényléshez használt eszköz
+    note?: string; //megjegyzés
+    dosage: string; //dózis, mikor, mennyit, hogyan
+    eventHistory: string; //mikor lett hozzáadva az adminisztráció
 }

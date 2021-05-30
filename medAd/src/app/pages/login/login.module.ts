435886cd5266c20a573fa279efdac48ddc0e65e1
boss1203 <h983246@stud.u-szeged.hu>
2021-05-30 21:39:54 +0200

    full

--- medAd/src/app/pages/login/login.module.ts
+++ medAd/src/app/pages/login/login.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    RouterModule
   ],
   exports: [LoginComponent,MatCardModule]
 })
--- medAd/src/environments/environment.prod.ts
+++ medAd/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyA12E0u4vfF-yT-O2G9dLKhbICNbLm6urI',
+  authDomain: 'webkert-714a0.firebaseapp.com',
+  projectId: 'webkert-714a0',
+  storageBucket: 'webkert-714a0.appspot.com',
+  messagingSenderId: '429317432963',
+  appId: '1:429317432963:web:47b700ec224ba8b34addb6',
+  measurementId: 'G-VSJRDWDL3X'
+  },
   production: true
 };
